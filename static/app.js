2014999c2d4050f5946fb4ca0ffc651ecc58108c
Daniel Friedman <dfriedman58@gmail.com>
2024-07-12 15:32:01 -0400

    bytebeat

--- static/app.js
+++ static/app.js
@@ -1,49 +1,61 @@
-const AudioContext = window.AudioContext || webkitAudioContext
-const todo = () => {}
-const byteBeatNode = todo()
+const AudioContext = window.AudioContext || webkitAudioContext;
+const todo = () => {};
+const byteBeatNode = todo();
 //byteBeatNode.connect(audioContext.destination)
 
-const exampleSource = "Math.sin(t / sampleRate * Math.PI * 2 * 220)"
+const exampleSource = "t * ((t>>12|t>>8)&63&t>>4)";
 
-const compileAudioWorklet = (audioCtx, source, label) => {
+const compileAudioWorklet = async (audioCtx, source, label) => {
   const sourceString = `
   class BbProcessor extends AudioWorkletProcessor {
     constructor() {
       super();
       this.globalSample = 0;
+
+      this.cachedValue = 0;
+      this.counter = 0;
+      this.heldSamples = 2;
     }
 
     process(inputs, outputs, parameters) {
       for (let s = 0; s < outputs[0][0].length; s++) {
-          const t = this.globalSample + s;
-          outputs[0][0][s] = ${source};
+
+          if (this.counter === 0) {
+            const t = this.globalSample;
+            this.globalSample += 1;
+            let out = (${source}) & 0xffff;
+            out /= 0xffff;
+            out *= 2;
+            out -= 1;
+            this.counter = this.heldSamples;
+            this.cachedValue = out;
+          }
+          this.counter--;
+
+          outputs[0][0][s] = this.cachedValue;
       }
-      this.globalSample += outputs[0][0].length
+      // this.globalSample += outputs[0][0].length
       return true
     }
   }
 
-  registerProcessor("BbProcessor" + ${label}, BbProcessor)
+  registerProcessor("BbProcessor${label}", BbProcessor)
   `.trim();
 
-  const blob = new Blob([sourceString], { type: "text/javascript"})
+  const blob = new Blob([sourceString], { type: "text/javascript" });
   const url = URL.createObjectURL(blob);
-  console.log({blob, url})
-  audioCtx.audioWorklet.addModule(url);
+  console.log({ blob, url });
+  await audioCtx.audioWorklet.addModule(url);
+};
 
-
-}
-
-
-
-
-
-const hasLoaded = new Promise((resolve, reject) => {
-  document.onclick = () => {
-    const audioContext = new AudioContext()
-    if (audioContext.state === 'suspended') {
-      audioContext.resume().then(resolve)
+const hasLoaded = new Promise(async (resolve, reject) => {
+  document.onclick = async () => {
+    const audioContext = new AudioContext();
+    if (audioContext.state === "suspended") {
+      audioContext.resume().then(resolve);
     }
-    compileAudioWorklet(audioContext, exampleSource, 0);
-  }
-})
+    await compileAudioWorklet(audioContext, exampleSource, 0);
+    workletNode = new AudioWorkletNode(audioContext, "BbProcessor0");
+    workletNode.connect(audioContext.destination);
+  };
+});
